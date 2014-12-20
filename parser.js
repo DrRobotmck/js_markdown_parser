6c9de655aa6804b249e64171bc268828a224d9d6
DrRobotmck <mckenneth.scott@icloud.com>
2014-12-20 14:43:10 -0500

    still working on ordered lists, refactored to minimize splitting and joining

--- parser.js
+++ parser.js
@@ -1,8 +1,7 @@
 function parser(input) {
 
 	var parseHeaders = function(inputString) {
-		var splitString = inputString.split('\n');
-		var parsedString = splitString.map(function(string, idx, splitUpString) {
+		var parsedString = inputString.map(function(string, idx, splitUpString) {
 			if ((/^#+/).test(string)) {
 				var headerDelimiter = string.match(/^#+/)[0];
 				var headerSize = headerDelimiter.length;
@@ -17,37 +16,36 @@ function parser(input) {
 				return string;
 			}
 		});
-		return parsedString.join('\n');
-	}
+		return parsedString;
+	};
 
 	var parseItalics = function(inputString) {
-		var splitString = inputString.split('\n');
-		var parsedString = splitString.map(function(string, idx, splitUpString) {
+		var parsedString = inputString.map(function(string, idx, splitUpString) {
 			if ((/(\s[*]{1}[^*_].{1,}[*]{1}\s*|\s[_]{1}[^*_].{1,}[_]{1}\s*)/).test(string)) {
 				string = string.replace(/\s[_*]{1}/g, ' <span class="md-italicize">');
 				string = string.replace(/[_*]{1}\s/g, '</span> ');
 			}
 			return string;
+
 		});
-		return parsedString.join('\n');
-	}
+		return parsedString;
+	};
 
 	var parseBolds = function(inputString) {
-		var splitString = inputString.split('\n');
-		var parsedString = splitString.map(function(string, idx, splitUpString) {
+		var parsedString = inputString.map(function(string, idx, splitUpString) {
 			if ((/([*]{2}[^*_].{1,}[*]{2}|[_]{2}[^*_].{1,}[_]{2})/).test(string)) {
 				string = string.replace(/[_*]{2}/g, '<strong>');
 				var indexOfLastTag = string.lastIndexOf('<strong>');
 				string = string.substring(0,indexOfLastTag) + '</strong>' + string.substring(indexOfLastTag + 8);
 			}
+
 			return string;
 		});
-		return	parsedString.join('\n');
-	}
+		return parsedString;
+	};
 
 	var parseImages = function(inputString) {
-		var splitString = inputString.split('\n');
-		var parsedString = splitString.map(function(string, idx, splitUpString) {
+		var parsedString = inputString.map(function(string, idx, splitUpString) {
 			if ((/!\[\w*\]\({1}https?:.+\){1}/).test(string)) {
 				var images = string.match(/!\[\w*\]\({1}https?:\/\/[\w.*-\/*]+\){1}/g);
 				var imagesLength = images.length;
@@ -62,14 +60,14 @@ function parser(input) {
 					string = string.replace(images[i], renderedImage[i]);
 				}
 			}
+
 			return string;
 		});
-		return parsedString.join('\n');
-	}
+		return parsedString;
+	};
 
 	var parseLinks = function(inputString) {
-		var splitString = inputString.split('\n');
-		var parsedString = splitString.map(function(string, idx, splitUpString) {
+		var parsedString = inputString.map(function(string, idx, splitUpString) {
 			if ((/!{0}\[.*\]\({1}https?:.+\){1}/).test(string)) {
 				var links = string.match(/!{0}\[.*\]\({1}https?:.+\){1}/);
 				var linksLength = links.length;
@@ -78,81 +76,82 @@ function parser(input) {
 					var linkText = linkString.slice(1,lastBracketIdx);
 					linkString = linkString.replace(/\[.*\]\({1}/, '<a href="');
 					linkString = linkString.replace(')', '">' + linkText + "</a>");
-					return linkString
+
+					return linkString;
 				});
 				for( var i = 0; i < linksLength; i++) {
 					string = string.replace(links[i], renderedLinks[i]);
 				}
 			}
+
 			return string;
 		});
-		return parsedString.join('\n');
-	}
+		return parsedString;
+	};
 
 	var parseUnorderedLists = function(inputString) {
-		var splitString = inputString.split('\n');
-		var parsedString = splitString.map(function(string, idx, splitUpString) {
-			var parseOpeningTag = function(regEx) {
+		var parsedString = inputString.map(function(string, idx, splitUpString) {
+			var parseTags = function(regEx) {
 				if (regEx.test(splitUpString[idx - 1])) {
 					string = string.replace(regEx, "<li>");
 				} else {
 					string = string.replace(regEx, "<ul><li>");
 				}
+				if ((/^[*-]\s/).test(splitUpString[idx + 1])) {
+					string += "</li>";
+				} else if (!(/^\s{2}[*-]\s/).test(splitUpString[idx + 1])) {
+					string += "</li></ul>";
+				}
 			};
 
 			if ((/^[*-]\s/).test(string)) {
-				parseOpeningTag(/^[*-]\s/);
+				parseTags(/^[*-]\s/);
 			} else if ((/^\s{2}[*-]\s/).test(string)) {
-				parseOpeningTag(/^\s{2}[*-]\s/);
+				parseTags(/^\s{2}[*-]\s/);
 			}
 
-			if ((/^[*-]\s/).test(splitUpString[idx + 1])) {
-				string += "</li>";
-			} else if (!(/^\s{2}[*-]\s/).test(splitUpString[idx + 1])) {
-				string += "</li></ul>";
-			}
 			return string;
 		});
-		return parsedString.join('\n');
-	}
+		return parsedString;
+	};
 
 	var parseOrderedLists = function(inputString) {
-		var splitString = inputString.split('\n');
-		var parsedString = splitString.map(function(string, idx, splitUpString) {
-			var parseOpeningTag = function(regEx) {
+		var parsedString = inputString.map(function(string, idx, splitUpString) {
+			var parseTags = function(regEx) {
 				if (regEx.test(splitUpString[idx - 1])) {
 					string = string.replace(regEx, "<li>");
 				} else {
 					string = string.replace(regEx, "<ol><li>");
 				}
+				if ((/^\d{1,}[.]\s/).test(splitUpString[idx + 1])) {
+					string += "</li>";
+				} else if (!(/^\s{2}\d{1,}[.]\s/).test(splitUpString[idx + 1])) {
+					string += "</li></ol>";
+				}
 			};
 
 			if ((/^\d{1,}[.]\s/).test(string)) {
-				parseOpeningTag(/^\d{1,}[.]\s/);
+				parseTags(/^\d{1,}[.]\s/);
 			} else if ((/^\s{2}\d{1,}[.]\s/).test(string)) {
-				parseOpeningTag(/^\s{2}\d{1,}[.]\s/);
+				parseTags(/^\s{2}\d{1,}[.]\s/);
 			}
 
-			if ((/^\d{1,}[.]\s/).test(splitUpString[idx + 1])) {
-				string += "</li>";
-			} else if (!(/^\s{2}\d{1,}[.]\s/).test(splitUpString[idx + 1])) {
-				string += "</li></ol>";
-			}
 			return string;
 		});
-		return parsedString.join('\n');
-	}
+		return parsedString;
+	};
 
 	var markdownParser = function(inputString) {
-		var parsedWithHeaders = parseHeaders(inputString);
+		var splitString = inputString.split('\n');
+		var parsedWithHeaders = parseHeaders(splitString);
 		var parsedWithItalics = parseItalics(parsedWithHeaders);
 		var parsedWithBolds = parseBolds(parsedWithItalics);
 		var parsedForImages = parseImages(parsedWithBolds);
 		var parsedForLinks = parseLinks(parsedForImages);
 		var parsedForUnorderedList = parseUnorderedLists(parsedForLinks);
-		var parsedForOrderedList = parseOrderedLists(parsedForUnorderedList);
-		return parsedForOrderedList;
-	}
+		var finalParsedString = parseOrderedLists(parsedForUnorderedList);
+		return finalParsedString.join('\n');
+	};
 
 	return markdownParser(input);
 }
@@ -160,5 +159,5 @@ function parser(input) {
 window.onload = function() {
 	document.getElementById('button').addEventListener('click', function() {
 		document.getElementById('viewer').innerHTML = parser(document.getElementById('text').value);
-	})
-}
+	});
+};
